eb7e71e29811e45d53fadc57c14f210230bf1388
Ellie <ellieN1235@gmail.com>
2025-07-01 19:20:48 -0700

    Initial Commit

---
+++ NeuroNote/NeuroNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroNoteApp.swift
+//  NeuroNote
+//
+//  Created by Ellie on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NeuroNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeuroNoteTests/NeuroNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroNoteTests.swift
+//  NeuroNoteTests
+//
+//  Created by Ellie on 7/1/25.
+//
+
+import Testing
+@testable import NeuroNote
+
+struct NeuroNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
